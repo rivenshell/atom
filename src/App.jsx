15f6f3043f8af39218e6ba2e100abd7a44924cf6
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-04-15 21:55:45 -0400

    Initial update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <>
-   <div className="App">
-      welcome to the simulation
+    <>
+      <div className="App">
+        <h1 class="title">welcome to the simulation</h1>
       </div>
-      </> 
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
